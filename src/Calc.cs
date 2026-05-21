c129c7f5f601d655dbdaede0336ef02927183810
Pratyush Pande <110541086+prpande@users.noreply.github.com>
2026-05-21 22:41:48 +0530

    advance head for stale-oid spec

--- src/Calc.cs
+++ src/Calc.cs
@@ -1,6 +1,2 @@
-namespace Sandbox;
-public static class Calc
-{
-    public static int Add(int a, int b) => a + b;
-    public static int Sub(int a, int b) => a - b;
-}
+// advanced 1779383507074
+public static int Mul(int a, int b) => a * b;
