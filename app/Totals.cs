5457f435d701a591e48359b271697341da91e412
Pratyush Pande <pratyush.pande@playlist.com>
2026-09-16 14:50:41 +0530

    build the sandbox library in CI instead of checking a marker file

---
+++ app/Totals.cs
@@ -0,0 +1,15 @@
+namespace Sandbox;
+
+public static class Totals
+{
+    public static int SumAll(int[] values)
+    {
+        int total = 0;
+        foreach (var entry in values)
+        {
+            total += value;
+        }
+
+        return total;
+    }
+}
